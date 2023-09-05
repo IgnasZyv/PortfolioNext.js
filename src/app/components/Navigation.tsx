1bb405835d620c91211f5b0a84a6c07880b0eb80
Ignas Zyvatkauskas <ignaszyv@gmail.com>
2023-09-05 21:41:05 +0300

    started about me section and navigation

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,20 @@
+
+
+export default function Navigation() {
+    return(
+        <>
+            <div className={"h-fit z-20"} style={{zIndex: 20}}>
+                <nav className={"p-5 mx-5 flex"}>
+                    <ul>
+                        <li>
+                            test
+                        </li>
+                    </ul>
+
+
+                </nav>
+                <button/>
+            </div>
+        </>
+    )
+}
